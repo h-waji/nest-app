78cfbe173182926c5e883a211650661d0c0a9ab0
hamru <goo.hiro.w@gmail.com>
2024-03-13 20:31:03 +0900

    feat: read, update, delete

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { Item } from './item.model';
 import { ItemsService } from './items.service';
 import { ItemStatus } from './item-status.enum';
@@ -8,8 +8,13 @@ export class ItemsController {
     constructor(private readonly itemService: ItemsService) {}
 
     @Get()
-    findAll() {
+    findAll(): Item[] {
         return this.itemService.findAll();
+    }
+
+    @Get(':id') // /items/hoge1
+    findById(@Param('id') id: string): Item {
+        return this.itemService.findById(id);
     }
 
     @Post()
@@ -27,5 +32,15 @@ export class ItemsController {
             status: ItemStatus.ON_SALE,
         }
         return this.itemService.create(item);
+    }
+
+    @Patch(':id')
+    updateStatus(@Param('id') id: string): Item {
+        return this.itemService.updateStatus(id);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): void {
+        this.itemService.delete(id);
     }
 }
