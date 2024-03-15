a334eb72d19c98aa3bb9b780c0efb42c45d6038b
hamru <goo.hiro.w@gmail.com>
2024-03-15 19:45:50 +0900

    feat: read from the database

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,13 +8,13 @@ export class ItemsController {
     constructor(private readonly itemService: ItemsService) {}
 
     @Get()
-    findAll(): Item[] {
-        return this.itemService.findAll();
+    async findAll(): Promise<Item[]> {
+        return await this.itemService.findAll();
     }
 
     @Get(':id') // /items/hoge1
-    findById(@Param('id', ParseUUIDPipe) id: string): Item {
-        return this.itemService.findById(id);
+    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+        return await this.itemService.findById(id);
     }
 
     @Post()
@@ -22,10 +22,10 @@ export class ItemsController {
         return await this.itemService.create(createItemDto);
     }
 
-    @Patch(':id')
-    updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
-        return this.itemService.updateStatus(id);
-    }
+    // @Patch(':id')
+    // updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
+    //     return this.itemService.updateStatus(id);
+    // }
 
     @Delete(':id')
     delete(@Param('id', ParseUUIDPipe) id: string): void {
