fed54613b3dea47b9cc6c9e94a49f5caca2df64a
hamru <goo.hiro.w@gmail.com>
2024-03-16 16:14:20 +0900

    feat: created the user table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ItemsModule,
     TypeOrmModule.forRoot(),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
