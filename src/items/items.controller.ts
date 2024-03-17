ba3ebbf1dce2d4b21f4b3163aab19dda4c6ee33a
hamru <goo.hiro.w@gmail.com>
2024-03-17 20:31:12 +0900

    fix: update and delete of items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Item } from '../entities/item.entity';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
@@ -7,6 +7,7 @@ import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { User } from 'src/entities/user.entity';
 
 @Controller('items')
+@UseInterceptors(ClassSerializerInterceptor)
 export class ItemsController {
     constructor(private readonly itemService: ItemsService) {}
 
@@ -27,18 +28,24 @@ export class ItemsController {
         @GetUser() user:User,
     ): Promise<Item> {
         console.log(user);
-        return await this.itemService.create(createItemDto);
+        return await this.itemService.create(createItemDto, user);
     }
 
     @Patch(':id')
     @UseGuards(JwtAuthGuard)
-    async updateStatus(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
-        return await this.itemService.updateStatus(id);
+    async updateStatus(
+        @Param('id', ParseUUIDPipe) id: string,
+        @GetUser() user: User,
+    ): Promise<Item> {
+        return await this.itemService.updateStatus(id, user);
     }
 
     @Delete(':id')
     @UseGuards(JwtAuthGuard)
-    async delete(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
-        await this.itemService.delete(id);
+    async delete(
+        @Param('id', ParseUUIDPipe) id: string,
+        @GetUser() user:User,
+    ): Promise<void> {
+        await this.itemService.delete(id, user);
     }
 }
